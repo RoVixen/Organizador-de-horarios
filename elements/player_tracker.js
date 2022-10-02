9a31ec9a932f9baf31a39466dadb4b832ce80ef7
RoVixen <griffertyuiop@gmail.com>
2022-10-02 00:25:13 -0400

    agregacion de los jugadores y su tiempo libre

--- elements/player_tracker.js
+++ elements/player_tracker.js
@@ -7,6 +7,36 @@ function PlayerTracker(props={}){
 
   let addingPlayer=false;
 
+  let playersSchedules={};
+
+  function renderPlayers(){
+
+    //genera elementos HTML que pasarian a se rlos nombres de los jugadores
+    document.querySelector(`#${thisId} #players #player_list`)
+    .innerHTML=Object.entries(playersSchedules)
+    .map(([nombre,schedule])=>{
+
+      //esta iteracion cuenta cuantas horas libres al dia tiene el jugador
+      const timeAviable=Object.values(schedule).reduce((prev,schedList,index)=>{
+        console.log(Array.isArray(schedList),schedList)
+        if(schedList===true)
+        return prev+24;
+
+        if(Array.isArray(schedList) && schedList.length>0)
+        return prev+schedList.length
+
+        return prev
+      },0)||0
+
+      //168 son la cantidad de horas que tiene una semana, de este calculo se saca un porcentaje de 
+      //tiempo libre a la semana que tiene el jugador
+      const percentTimeAviable=Math.floor(100*(timeAviable/168))+"%"
+
+      return `<h1>${nombre} (libre ${percentTimeAviable} del tiempo)</h1>`
+    })
+    .join("")
+  }
+
   setTimeout(()=>{
 
     const addPlayerElem=document.querySelector(`#${thisId} #adding_player`)
@@ -16,6 +46,15 @@ function PlayerTracker(props={}){
     .addEventListener("click",(e)=>{
 
       function acceptHandler(playerFreeDays){
+        const playerName=document.querySelector(`#${thisId} #adding_player input#player_name`).value
+
+        if(playerName=="")
+        return alert("Colocale un nombre al jugador");
+        
+        playersSchedules[playerName]=playerFreeDays;
+
+        renderPlayers();
+        
         setAdding(false);
       }
 
@@ -39,6 +78,7 @@ function PlayerTracker(props={}){
     <section id="players">
       <div id="actions" class="actions">
         <button id="agregar">Agregar Jugador</button>
+        <div id="player_list"></div>
       </div>
     </section>
     <section id="adding_player"></section>
